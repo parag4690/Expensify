f44f4871549b174106aec1ef5ed30316fc340383
paragSharma <parag0992.be21@chitkara.edu.in>
2024-12-10 10:54:20 +0530

    expensify

---
+++ backend/DB/Database.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+// import env from dotenv;
+import dotenv from 'dotenv';
+dotenv.config();
+export const connectDB = async (req, res) => {
+    const db = process.env.MONGO_URL;
+ 
+    const {connection} = await mongoose.connect(db, { useNewUrlParser: true });
+
+    console.log(`MongoDB Connected to ${connection.host}`);
+
+}
